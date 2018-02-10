b8a66fcc8952a2ad443fea4276ac5812b12f27e3
Hafizh Afkar Makmur <hafizhmakmur@gmail.com>
2018-02-11 03:40:56 +0700

    Add test in main

--- src/main/java/id/ac/itb/stei/fair/cipher/gui/Main.java
+++ src/main/java/id/ac/itb/stei/fair/cipher/gui/Main.java
@@ -11,6 +11,8 @@ import java.util.BitSet;
 public class Main {
     
     public static void main(String [] args) {
+        BpcsStega test = new BpcsStega();
+        
 	byte[] bytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".getBytes();
 	BitSet[] res = BpcsStega.preprocessInput(bytes, 0);
 	byte[] postprocessOutput = BpcsStega.postprocessOutput(res);
